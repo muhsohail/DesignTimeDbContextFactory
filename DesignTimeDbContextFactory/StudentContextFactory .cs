9cca3a3634846ad1f3d1b126d8f41fc2cc553024
Muhammad Sohail <mirza.sohail@hotmail.com>
2019-01-30 17:04:58 +0400

    Implemented IDesignTimeDbContext

---
+++ DesignTimeDbContextFactory/StudentContextFactory .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DesignTimeDbContextFactory
+{
+    public class StudentContextFactory : IDesignTimeDbContextFactory<StudentContext>
+    {
+        public StudentContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StudentContext>();
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=SampleDesignTimeDbContext;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new StudentContext(optionsBuilder.Options);
+        }
+    }
+}
